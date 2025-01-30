0c442b28b9e1279111e8aec429ae5e52e49cb451
cyubahiro Richard <cyubahirorichard250@gmail.com>
2025-01-30 13:58:32 -0800

    passport

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express  = require('express');
 const app = express();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
 const initializePassport = require('./passport-config');
 initializePassport(
     passport, 
-    email => users.find(user => user.emai === email))
-
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 const users = []
  
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+})) 
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Richard'})
+    res.render('index.ejs', { name: req.user.name})
 });
 
-app.get('/login', (req, res) => {
-res.render('login.ejs');
+app.get('/login', (req,res) => {
+   res.render('login.ejs');
 });
 
 app.get('/register', (req, res) => {
 res.render('register.ejs');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
 
-});
+
 app.post('/register', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
